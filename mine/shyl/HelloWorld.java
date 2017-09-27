4924b88c18f1e4d2f43e264c553964ccd04e38c3
hyghub <h86440001@163.com>
2017-09-27 13:15:25 +0800

    Update HelloWorld.java
    use a private method to print something.

--- mine/shyl/HelloWorld.java
+++ mine/shyl/HelloWorld.java
@@ -2,6 +2,10 @@ package mine.shyl;
 
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("Hello World !");
+		println("Hello World !");
+	}
+	
+	private static void println(String someThing){
+		System.out.println(someThing);
 	}
 }
