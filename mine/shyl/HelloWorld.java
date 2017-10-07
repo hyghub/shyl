f1921a27f9bdd636dffb501e6d18bbab15649c35
hyghub <h86440001@163.com>
2017-10-08 01:24:56 +0800

    say another hello

--- mine/shyl/HelloWorld.java
+++ mine/shyl/HelloWorld.java
@@ -3,6 +3,7 @@ package mine.shyl;
 public class HelloWorld{
 	public static void main(String[] args){
 		println("Hello World !");
+		println("Hello World !!");
 	}
 	
 	private static void println(String someThing){
